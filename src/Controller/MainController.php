8e681e60ebafa19a7b8865b67ff61b890b0cb2b2
Laura LOspitao <lauralospitao@gmail.com>
2020-07-21 10:47:25 +0200

    #1 Create materialize Navbar structure

---
+++ src/Controller/MainController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('main/index.html.twig');
+    }
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request) {
+        $name=$request->get('name');
+        return $this->render('main/custom.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
